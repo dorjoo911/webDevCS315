980ea6b9f7db36960635b589b0dbb0dce6b691ec
dorjoo911 <tomike@asia.com>
2022-04-16 09:09:15 -0500

    fix

--- inventory/script.js
+++ inventory/script.js
@@ -1,11 +1,36 @@
 window.onload = pageLoaded;
-
+let productData = [];
 let buttonEl;
 
 function pageLoaded() {
+  let sortBtn = document.querySelector(
+    "#myTable > tbody > tr > th:nth-child(5) > select > option:nth-child(2)"
+  );
+  let filterBtn = document.querySelector(
+    "#myTable > tbody > tr > th:nth-child(5) > select > option:nth-child(3)"
+  );
+  let sortFilt = document.querySelector(
+    "#myTable > tbody > tr > th:nth-child(5) > select"
+  );
   buttonEl = document.getElementById("inventtoryBtn");
   buttonEl.onclick = createTable;
+
+  if (sortBtn.selected) {
+    sortBtn.addEventListener("change", function (el) {
+      productData.proName.sort();
+    });
+  } else if (filterBtn.selected) {
+    filterBtn.addEventListener("change", function (el) {
+      priceData.forEach(function (element) {
+        if (element.price > 100) {
+          element.remove();
+        }
+      });
+    });
+  }
 }
+
+// let selected = { sortSelected: false, filterSelected: false };
 
 function createTable() {
   let table = document.getElementById("myTable");
@@ -37,6 +62,7 @@ function createTable() {
     document.querySelector("#inputQuantidy_4").style.background = "red";
     deleteRow();
   }
+
   let rating = document.getElementById("inputRaiting_4").value;
   if (rating == !Number) {
     alert("Please input number only!!!");
@@ -49,7 +75,7 @@ function createTable() {
   }
   let update =
     '<button class="inventoryBtn" type="button" id="editBtn" onclick="editRow(this)">Edit</button>' +
-    '<button class="inventoryBtn" type="submit" id="saveBtn" onclick="saveThenEdit(this) display="none" >Save</button>';
+    '<button class="inventoryBtn" type="submit" id="saveBtn" onclick="saveThenEdit(this) >Save</button>';
   let cancel =
     '<button class="inventoryBtn" type="button" id="deleteBtn" onclick="deleteRow(this)">Delete</button>';
 
@@ -59,6 +85,13 @@ function createTable() {
   cell4.innerHTML = rating;
   cell5.innerHTML = update;
   cell6.innerHTML = cancel;
+
+  const temp = {
+    proName: product,
+    price: quantidy,
+  };
+  productData.push(temp);
+  console.log("productData :>> ", productData);
 }
 
 function deleteRow(row) {
@@ -85,10 +118,3 @@ function editRow(obj) {
   rating.innerHTML =
     "<input type='text' id='name_text' value='" + rating_val + "'>";
 }
-// function saveThenEdit(obj) {
-//   // let itemName = document.querySelector("#name_text").value;
-//   let catName = obj.parentNode.parentNode.children[1];
-//   let quantity = obj.parentNode.parentNode.parentNode.children[2];
-//   let rating = obj.parentNode.parentNode.parentNode.children[3];
-//   console.log("itemName :>> ", catName);
-// }
