7ffc03165493f9b5993ae86f146479dd54d53118
dorjoo911 <tomike@asia.com>
2022-04-20 20:05:18 -0500

    all pages created

--- CS315_Project/app.js
+++ CS315_Project/app.js
@@ -12,3 +12,42 @@ window.addEventListener("load", function () {
     else eachOption.selected = false;
   }
 });
+
+/* ------------- Inventory ------------- */
+let dataBase = [
+  { product: "apple", category: "electronic", price: 99, rating: 5 },
+  { product: "pen", category: "kids", price: 9, rating: 5 },
+  { product: "desc", category: "office", price: 99, rating: 4 },
+];
+
+function generateItems() {
+  let item = {};
+  // get items
+  dataBase.push(item);
+}
+
+/* ----------- show items in product.html ----------- */
+
+dataBase.forloop(function (dataBase) {
+  let name = document.querySelector(
+    "body > div.container > div.cat1.card > h2"
+  );
+  let image = document.querySelector(
+    "body > div.container > div.cat1.card > a > img"
+  );
+  let price = document.querySelector(
+    "body > div.container > div.cat1.card > div > div > h4"
+  );
+  let rating = document.getElementById("idName");
+  let description = document.querySelector(
+    "body > div.container > div.cat1.card > div > p"
+  );
+  let category = document.querySelector(
+    "body > div.container > div.cat1.card > div > div > label"
+  );
+
+  image.innerHTML = dataBase.itemName.value;
+  price.innerHTML = dataBase.itemPrice.value;
+  rating.innerHTML = dataBase.itemRating.value;
+  description.innerHTML = dataBase.itemDesc.value;
+});
